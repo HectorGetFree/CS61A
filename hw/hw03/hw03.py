0211660d8704d4c00b2386088d60d2ad2fa595cb
Hector <hectorgetfree@gmail.com>
2025-05-20 21:09:02 +0800

    complete hw03 task1

--- hw/hw03/hw03.py
+++ hw/hw03/hw03.py
@@ -25,6 +25,12 @@ def num_eights(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    if n == 0:
+        return 0
+    if n % 10 == 8:
+        return 1 + num_eights(n // 10)
+    else:
+        return num_eights(n // 10)
 
 
 def digit_distance(n):
