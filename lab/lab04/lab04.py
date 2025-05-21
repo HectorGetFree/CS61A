713333c005279e3ba00ca48dc09a61dc0092b303
Hector <hectorgetfree@gmail.com>
2025-05-21 20:31:26 +0800

    complete lab04 q2

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -7,7 +7,7 @@ def divide(quotients, divisors):
     >>> divide(range(1, 5), range(20, 25))
     {1: [20, 21, 22, 23, 24], 2: [20, 22, 24], 3: [21, 24], 4: [20, 24]}
     """
-    return {____: ____ for ____ in ____}
+    return {i : [s for s in divisors if s % i == 0] for i in quotients}
 
 
 def buy(fruits_to_buy, prices, total_amount):
