2c13a35bb011d293f12d53cc4bdb0369affeaeef
Hector <hectorgetfree@gmail.com>
2025-05-26 09:28:14 +0800

    complete scheme proj p2

--- proj/scheme/scheme_eval_apply.py
+++ proj/scheme/scheme_eval_apply.py
@@ -44,11 +44,19 @@ def scheme_apply(procedure, args, env):
        assert False, "Not a Frame: {}".format(env)
     if isinstance(procedure, BuiltinProcedure):
         # BEGIN PROBLEM 2
-        "*** YOUR CODE HERE ***"
+        # 得到py参数列表
+        arg_py_list = []
+        pointer = args
+        while pointer is not nil:
+            arg_py_list.append(pointer.first)
+            pointer = pointer.rest
+        # 判断是否要加上env
+        if procedure.need_env:
+            arg_py_list.append(env)
         # END PROBLEM 2
         try:
             # BEGIN PROBLEM 2
-            "*** YOUR CODE HERE ***"
+            return procedure.py_func(*arg_py_list)
             # END PROBLEM 2
         except TypeError as err:
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
